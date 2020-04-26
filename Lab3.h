b18703e1d527ef53f0de7c7cb6aceaea465436db
Forgefill <63472004+Forgefill@users.noreply.github.com>
2020-04-26 14:25:28 +0300

    Add files via upload

---
+++ Lab3.h
@@ -0,0 +1,11 @@
+#ifndef Lab3
+#define Lab3
+
+int hello();
+void filling(int &, int, int&);
+void swap(int &, int &);
+int add();
+int step2();
+void sortswap(int &, int &);
+void output(int &, int &)
+#endif // !Lab3
